b087b1fe836d43f4eb228505ff48bab7bd1516fb
Duncan <9828842+legowerewolf@users.noreply.github.com>
2025-06-07 15:59:03 +0000

    ao3fetch: add 2.1.0 bottle.

--- Formula/a/ao3fetch.rb
+++ Formula/a/ao3fetch.rb
@@ -4,6 +4,13 @@ class Ao3fetch < Formula
   url "https://github.com/legowerewolf/AO3fetch.git", tag: "v2.1.0", revision: "e96157c81e4de8fc1b7440101301d6479f621024"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/legowerewolf/homebrew-tap/releases/download/ao3fetch-2.1.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05ffe1e401649fa3680a357a86def53c2aa4d4ca04160e383c5a6fcbdff79138"
+    sha256 cellar: :any_skip_relocation, ventura:       "2f036b552bd80df2f5f2016b0f757b9f613fa79187c88913cfa5045645f49902"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c7dcc423739fc5846190d9c5eaa295c1dc95f775c092ab9d0da20984dcac9f36"
+  end
+
   depends_on "go" => :build
 
   def install
