e9252ee366787bcc5b5f5e85b3d290818f103f71
Duncan <9828842+legowerewolf@users.noreply.github.com>
2025-06-12 01:32:12 +0000

    ao3fetch: update 2.1.1 bottle.

--- Formula/a/ao3fetch.rb
+++ Formula/a/ao3fetch.rb
@@ -5,10 +5,10 @@ class Ao3fetch < Formula
   license "MIT"
 
   bottle do
-    root_url "https://github.com/legowerewolf/homebrew-tap/releases/download/ao3fetch-2.1.0"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "05ffe1e401649fa3680a357a86def53c2aa4d4ca04160e383c5a6fcbdff79138"
-    sha256 cellar: :any_skip_relocation, ventura:       "2f036b552bd80df2f5f2016b0f757b9f613fa79187c88913cfa5045645f49902"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "c7dcc423739fc5846190d9c5eaa295c1dc95f775c092ab9d0da20984dcac9f36"
+    root_url "https://github.com/legowerewolf/homebrew-tap/releases/download/ao3fetch-2.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "88fee2206eb05b4253f10245f871b724a7466a2c61fe740798705b19445ce36e"
+    sha256 cellar: :any_skip_relocation, ventura:       "4532102e4e7636b7a40eee741acab37e1f87a7c0aec84893ccafc026876902d4"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "56db51f38129afba49e9de8ac5d069f35941dc1944b70979eb7a5bb4b7b20c7c"
   end
 
   depends_on "go" => :build
